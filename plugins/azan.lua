11cd6cf451d8d66e188fde3b2245ea7565231035
TEAMSOFTTG <tgteam351@gmail.com>
2016-05-07 14:02:08 +0430

    Added files via upload

--- plugins/azan.lua
+++ plugins/azan.lua
@@ -1,3 +1,11 @@
+--[[
+
+#
+#     @GPMOD
+#   @Dragon_Born
+#      
+
+]]
 do
 function run_bash(str)
     local cmd = io.popen(str)
@@ -54,7 +62,7 @@ function run(msg, matches)
 	redis:incr(hash)
 	local receiver	= get_receiver(msg)
 	local city = matches[1]
-	if matches[1] == 'azan' then
+	if matches[1] == 'praytime' then
 	city = 'Tehran'
 	end
 	local lat,lng,url	= get_staticmap(city)
@@ -85,7 +93,7 @@ function run(msg, matches)
 end
 
 return {
-  patterns = {"^[/#!][Aa]zan (.*)$","^[/#!](azan)$"}, 
+  patterns = {"^[/!][Pp]raytime (.*)$","^[/!](praytime)$"}, 
   run = run 
 }
 
