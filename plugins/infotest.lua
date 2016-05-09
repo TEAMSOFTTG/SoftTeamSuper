f0226e329617480e62d47ad9d94602207647039d
TEAMSOFTTG <tgteam351@gmail.com>
2016-05-10 00:14:24 +0430

    Add files via upload

---
+++ plugins/infotest.lua
@@ -0,0 +1,17 @@
+do
+function run(msg, matches)
+local reply_id = msg['id']
+local text = 'info'
+local text1 ="🔱group ID : "..msg.to.id.."\n🔱group name : "..msg.to.title.."\n🔱your name : "..(msg.from.first_name or '').."\n🔱first : "..(msg.from.first_name or '').."\n🔱last : "..(msg.from.last_name or '').."\n🔱user ID :
+ "..msg.from.id.."\n🔱username : @"..(msg.from.username or '').."\n🔱phone number : +"..(msg.from.phone or '').."/nایدی_پیام_تو > "..msg.id.."\nنوع سیمکارت > "..(msg.from.sim or ").."\n زمان > "..(msg.from.loc or ")"
+  reply_msg(reply_id, text1, ok_cb, false)
+end
+return {
+  description = "", 
+  usage = "",
+  patterns = {
+    "^[!/#]infotest$",
+  },
+  run = run
+}
+end
